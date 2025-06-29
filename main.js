5353caaa8fbbf49d9922a33d5617c712fef278c9
OllaWilk <alex.dev.wilk@gmail.com>
2025-06-29 12:33:56 +0200

    Fix mobile navigation display and add backdrop under menu

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import 'bootstrap';
 import './styles/main.scss';
+
+const backdrop = document.getElementById('backdrop');
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+     setTimeout(() => {
+    backdrop.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+  }, 10);
+});
+
+backdrop.addEventListener('click', () => {
+  hamburger.click(); 
+});
